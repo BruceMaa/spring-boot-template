ec173752ecbf58d187003606d1400e6e16a1aca2
BruceMaa <bruce.maqiang@gmail.com>
2019-11-15 11:52:33 +0800

    Spring Boot Init
    Spring Boot 项目初始化

---
+++ src/main/java/cn/brucemaa/springboot/template/SpringBootTemplateApplication.java
@@ -0,0 +1,16 @@
+package cn.brucemaa.springboot.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Bruce
+ */
+@SpringBootApplication
+public class SpringBootTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTemplateApplication.class, args);
+    }
+
+}
