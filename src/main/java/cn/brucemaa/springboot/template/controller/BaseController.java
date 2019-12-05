6ddf5f9cb99ae47f971533db2894106cbe67d625
BruceMaa <bruce.maqiang@gmail.com>
2019-12-05 19:08:39 +0800

    Create BaseController.java
    业务基础Controller

---
+++ src/main/java/cn/brucemaa/springboot/template/controller/BaseController.java
@@ -0,0 +1,12 @@
+package cn.brucemaa.springboot.template.controller;
+
+/**
+ * projectName:spring-boot-template
+ * cn.brucemaa.springboot.template.controller
+ * 基础Controller
+ * @author Bruce Maa
+ * @since 2019/12/5.15:19
+ */
+
+public class BaseController {
+}
